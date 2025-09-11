756e05c2dc0c471d688cf212bbb07bb6411b8f56
Doctor No.13 <hujiashuo2004@163.com>
2025-09-12 00:22:19 +0800

    第三章学习完毕

--- 第三章 分支结构.py
+++ 第三章 分支结构.py
@@ -1,5 +1,7 @@
 '''
 day2 20250911
+
+条件为bool类型,为True时才执行语句块。
 '''
 
 
@@ -71,5 +73,84 @@ else:
     print('不及格')
 '''
 
+'''
+无限循环
+while <条件>:
+    <语句块>
+
+i = 10
+while i>0 :
+    print('好'*i)
+    i -= 1
+
+这里i-=1的位置很重要
 
 
+i = 100
+sum = 0
+while i>0:
+    sum = sum+i
+    i -= 1
+print(sum)
+'''
+
+
+
+'''
+continue 和 break
+continue 用来结束当前本次循环
+continue 后面的内容不会执行然后继续执行下一次循环。
+
+break 用于终止所在的循环
+
+
+
+i = 0
+while i <= 100:
+    i = i + 1
+    if i % 2 == 0:
+        continue
+    print(i)
+
+count = 0
+i = 0
+while i <= 100:
+    i = i + 1
+    if i % 2 == 0:
+        continue
+    print(i)
+    count += 1
+    if count == 5:
+        break
+
+
+
+'''
+
+
+
+'''
+异常处理
+try - except
+
+try:
+    <语句块1>
+except:
+    <语句块2>
+
+try:
+    score = float (input ('请输入一段数字'))
+    print(score)
+except:
+    print('输入的是非数字')
+
+
+try:
+    a = 5
+    b = 0
+    print(a / b)
+except ZeroDivisionError:
+    print("除以零了！！")
+except:
+    print("出错了")
+'''
